570a75c32a9c8c1722625a1675cd6abba9fd912a
Madhurapetare <madhurapetare4705@gmail.com>
2025-11-15 23:21:00 +0530

    Create Booking.java

---
+++ Booking.java
@@ -0,0 +1,14 @@
+package com.example.flightapp.model;
+
+public class Booking {
+    public int id;
+    public int seats;
+    public double total_price;
+    public String booking_time;
+    public String airline;
+    public String flight_number;
+    public String src;
+    public String dest;
+    public String depart_datetime;
+    public String arrive_datetime;
+}
